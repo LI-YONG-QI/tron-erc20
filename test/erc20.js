6c0804a5f9954814a124edd09b894a3a86275c6f
LI-YONG-QI <a2369028@gmail.com>
2024-09-12 16:28:40 +0800

    test: erc20

--- test/erc20.js
+++ test/erc20.js
@@ -4,17 +4,7 @@ contract("ERC20", (accounts) => {
   it("Deploy", async () => {
     let token = await erc20.deployed();
 
-    console.log(token.abi);
-
     console.log(await token.symbol());
-
-    // const name = await token.name();
-    // const symbol = await token.symbol();
-
-    // assert.equal(name, "TEST");
-    // assert.equal(symbol, "test");
-
-    //let balance = await instance.getBalance.call(accounts[0]);
-    //assert.equal(balance.valueOf(), 10000);
+    console.log(await token.name());
   });
 });
